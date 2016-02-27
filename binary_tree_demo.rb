fea5e8de1154e3c00c4ff964c3b4131df5012e3a
Roger Banks <roger_banks@mac.com>
2016-02-27 17:31:58 -0600

    Initial checkin

---
+++ binary_tree_demo.rb
@@ -0,0 +1,8 @@
+require 'grid'
+require 'binary_tree'
+
+grid = Grid.new(10,10)
+BinaryTree.on(grid)
+puts grid
+img = grid.to_png
+img.save "maze.png"
---
+++ sidewinder_demo.rb
@@ -0,0 +1,8 @@
+require 'grid'
+require 'sidewinder'
+
+grid = Grid.new(10,10)
+
+Sidewinder.on(grid)
+
+puts grid
